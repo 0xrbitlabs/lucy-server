5dcc68f001fa00f6515ebe8ee4417ad08f803a92
TheWisePigeon <zozozozeph@gmail.com>
2024-01-12 14:49:53 +0000

    setup migrate script in Makefile and update user model

--- internal/types/user.go
+++ internal/types/user.go
@@ -2,7 +2,13 @@ package types
 
 type User struct {
 	Id             string `json:"id" db:"id"`
+	UserType       string `json:"user_type" db:"user_type"`
 	PhoneNumber    string `json:"phone_number" db:"phone_number"`
-	FullName       string `json:"full_name" db:"full_name"`
+	Password       string `json:"passord" db:"passord"`
+	Verified       bool   `json:"verified" db:"verified"`
+	Name           string `json:"full_name" db:"full_name"`
 	ProfilePicture string `json:"profile_picture" db:"profile_picture"`
+	Description    string `json:"descriptiton" db:"descriptiton"`
+	Country        string `json:"country" db:"country"`
+	Town           string `json:"town" db:"town"`
 }
