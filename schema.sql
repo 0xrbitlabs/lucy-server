b9248a2986014b23adc3be455deaa930fc4ddeda
TheWisePigeon <zozozozeph@gmail.com>
2024-02-02 16:07:05 +0000

    updated database schema

--- schema.sql
+++ schema.sql
@@ -6,38 +6,11 @@ create table admins (
 
 create table users (
   id text not null primary key,
-  user_type text not null default 'seller',
+  user_type text not null,
   phone_number text not null unique,
   password text not null,
   name text not null,
-  profile_picture text not null default 'https://picsum.photos/200/300',
   description text not null,
   country text not null,
   town text not null
-);
-
-create table categories (
-  id text not null primary key,
-  name text not null unique,
-  scope text not null default 'public'
-);
-
-create table products (
-  id text not null primary key,
-  user_id text not null references users(id),
-  category_id text not null references categories(id),
-  label text not null,
-  image text not null,
-  description text not null,
-  price numeric not null,
-  deliverable boolean not null default true,
-  sold_out boolean not null default false
-);
-
-create table verification_codes (
-  id text not null primary key,
-  code text not null,
-  sent_to text not null,
-  sent_at timestamp not null,
-  used boolean not null default false
 );
