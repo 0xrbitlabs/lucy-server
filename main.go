b6b8f2e6979b4b5d2cade549206f4c0b436d956c
TheWisePigeon <zozozozeph@gmail.com>
2024-04-04 11:49:47 +0000

    refactoring

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	"log/slog"
 	"net/http"
 	"os"
-	"server/types"
 )
 
 func main() {
@@ -21,21 +19,12 @@ func main() {
 	}
 	textHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{})
 	logger := slog.New(textHandler)
-	_ = logger
-	r := chi.NewRouter()
+	mux := http.NewServeMux()
+	mux.Handle("POST /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-	r.Route("/hook", func(r chi.Router) {
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			inboundMessage := types.NewInboundMessage(r)
-			fmt.Printf("%+v", inboundMessage)
-			return
-		})
-	})
-
-	r.Route("/auth", func(r chi.Router) {
-	})
+	}))
 	fmt.Println("Server launched on port 8081")
-	err := http.ListenAndServe(":8081", r)
+	err := http.ListenAndServe(":8081", mux)
 	if err != nil {
 		panic(err)
 	}
