dea56147858cc4d0a21ad373f88df49955188164
TheWisePigeon <zozozozeph@gmail.com>
2024-04-16 12:03:31 +0000

    implement auth middleware

--- store/admins.go
+++ store/admins.go
@@ -7,6 +7,11 @@ import (
 	"lucy/types"
 )
 
+type GetAdminFilter struct {
+	Column string
+	Value  string
+}
+
 func (s *Store) InsertAdmin(admin types.Admin) error {
 	_, err := s.db.NamedExec(
 		`
@@ -21,18 +26,19 @@ func (s *Store) InsertAdmin(admin types.Admin) error {
 	return nil
 }
 
-func (s *Store) GetAdminByUsername(username string) (*types.Admin, error) {
+func (s *Store) GetAdmin(filter GetAdminFilter) (*types.Admin, error) {
 	admin := &types.Admin{}
+	query := fmt.Sprintf("select * from admins where %s=$1", filter.Column)
 	err := s.db.Get(
 		admin,
-		"select * from admins where username=$1",
-		username,
+		query,
+		filter.Value,
 	)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, app_errors.ErrResourceNotFound
 		}
-		return nil, fmt.Errorf("Error while retrieving admin by username: %w", err)
+		return nil, fmt.Errorf("Error while retrieving admin by %s: %w", filter.Column, err)
 	}
 	return admin, nil
 }
