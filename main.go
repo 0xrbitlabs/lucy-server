bfb7258408c0b261353b91321df87710c71ee312
TheWisePigeon <zozozozeph@gmail.com>
2024-05-13 21:46:28 +0000

    feat: create and get all categories

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -25,6 +26,17 @@ func main() {
 	port := os.Getenv("PORT")
 
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Use(
 		middleware.Recoverer,
--- schema.sql
+++ schema.sql
@@ -15,3 +15,7 @@ create table if not exists categories (
   description text not null,
   enabled boolean not null
 );
+
+INSERT INTO users (id, username, phone, password, account_type, description, country, town) 
+VALUES 
+('1', 'admin_user', '123456789', '$2y$10$FtxxyQXJjwwST2tmKppQf.CArH84ekTLG3Ko0VXeH3vBOQC.2yrU2', 'admin', 'Administrator account', 'USA', 'New York');
