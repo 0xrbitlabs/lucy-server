d1da6511d5bb14a82055238fe3bcdc755881b1e0
joseph0x45 <zozozozeph@gmail.com>
2024-10-26 16:08:34 +0000

    remove old code

--- models/category.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Category struct {
-	ID          string `json:"id" db:"id"`
-	Label       string `json:"label" db:"label"`
-	Description string `json:"description" db:"description"`
-	Enabled     bool   `json:"enabled" db:"enabled"`
-}
--- seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (id, username, phone, password, account_type, description, country, town) 
-VALUES 
-('1', 'admin_user', '123456789', '$2y$10$FtxxyQXJjwwST2tmKppQf.CArH84ekTLG3Ko0VXeH3vBOQC.2yrU2', 'admin', 'Administrator account', 'USA', 'New York');
