13a1d089cdfa106f11f7c2fc5ea008fcddeef9ca
joseph0x45 <zozozozeph@gmail.com>
2024-11-23 15:51:08 +0000

    add seeding data

--- seed.sql
+++ seed.sql
@@ -1,5 +1,5 @@
 -- create admin user
--- password is 'password'
+-- password is always 'password'
 insert into users (
   id, phone, username, password, account_type
 )
@@ -7,4 +7,14 @@ values (
   '1', '0', 'admin1',
   '$2y$10$xBJcodAk96RUkv6tKqXLXuC0xl3/PTQxhY.oEiMirZ14PsaBksCjO',
   'admin'
-);
+) on conflict do nothing;
+
+-- create seller account
+insert into users (
+  id, phone, username, password, account_type
+)
+values (
+  '2', '1', 'seller1',
+  '$2y$10$xBJcodAk96RUkv6tKqXLXuC0xl3/PTQxhY.oEiMirZ14PsaBksCjO',
+  'seller'
+) on conflict do nothing;
