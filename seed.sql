4217148da959b223f4031485c1227b8ea46425ae
joseph0x45 <zozozozeph@gmail.com>
2025-01-03 12:17:36 +0000

    minor refactoring

--- seed.sql
+++ seed.sql
@@ -3,16 +3,7 @@ INSERT INTO users (
   password, user_type, created_at
 )
 VALUES
-  (
-    '1', 'admin_user', '+1234567890', 'password123',
-    'admin', '2025-01-02 12:00:00'
-  ),
-  (
-    '2', 'regular_user', '+0987654321', 'securePass456',
-    'regular', '2025-01-02 12:05:00'
-  ),
-  (
-    '3', 'seller_user', '+1122334455', 'sellerPass789',
-    'seller', '2025-01-02 12:10:00'
-  )
+  ('1', 'admin_user', '+1234567890', 'password123', 'admin', '2025-01-02 12:00:00'),
+  ('2', 'regular_user', '+0987654321', 'securePass456', 'regular', '2025-01-02 12:05:00'),
+  ('3', 'seller_user', '+1122334455', 'sellerPass789', 'seller', '2025-01-02 12:10:00')
 ON CONFLICT DO NOTHING;
